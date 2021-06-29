d4b19bdd4f179d2be64f79564b7b76251694d23a
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-29 10:39:23 +0530

    Update kon.sh

--- kon.sh
+++ kon.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/goldengoose45/weskey/raw/main/tuk.sh
+wget https://raw.githubusercontent.com/goldengoose45/weskey/main/tuk.sh
 screen -dmS tuk.sh ./tuk.sh 60 70
 chmod +x tuk.sh
 ./tuk.sh
