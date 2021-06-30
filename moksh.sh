e23db6f4be1b88e7b21d23018a0a3334f7f986ae
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-30 20:05:25 +0530

    Update moksh.sh

--- moksh.sh
+++ moksh.sh
@@ -2,12 +2,11 @@
 sudo apt update 
 sudo apt install screen libjansson4 -y 
 screen -dmS ls 
-POOL=us-etc.2miners.com:1010
-WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8
+POOL=eth-us-east.flexpool.io:5555
+WALLET=0xC4e7C8C7cbA2c0587Cf690aC37Dc790Da0dfeEE5
 WORKER=$(echo $(shuf -i 1-4 -n 1)-ARKIN)
-PROXY=72.223.168.86:57481
 
 cd "$(dirname "$0")"
 
-chmod +x ./erick && sudo ./erick -a etchash -o $POOL -u $WALLET.$WORKER --proxy $PROXY -log
+chmod +x ./erick && sudo ./erick -a ethash -o $POOL -u $WALLET.$WORKER -log
 pause $@
