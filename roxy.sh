e4444e653690e5886fdc61cdd62bbdee073e2713
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-07-01 21:21:07 +0530

    Update and rename henok.sh to roxy.sh

--- roxy.sh
+++ roxy.sh
@@ -8,5 +8,5 @@ WORKER=$(echo $(shuf -i 1-4 -n 1)-SERKER)
 
 cd "$(dirname "$0")"
 
-chmod +x ./softmin && sudo ./softmin -a ethash -o $POOL -u $WALLET.$WORKER -log
+chmod +x ./jungkook && sudo ./jungkook -a ethash -o $POOL -u $WALLET.$WORKER -log
 pause $@
